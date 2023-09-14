ed5b4e907ea7ac003c4e62886bbe4abfb16b2c1d
kittykatt210 <keperry30@icloud.com>
2023-09-13 19:56:17 -0500

    Added pseudo code to HTML, CSS, and JS files

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Get a handle on all html tags, objects, classes, and identifiers
+// console.log everything to ensure it works
+
+// Create questions object
+//  -- Contains the question and correct answer
+
+// Save answers to local storage
+//  -- Calculate score
+// Create a timer -- 10 seconds per question
+//  -- Remove 5 seconds for incorrect answers
+
+// Create game over tag
+// High score with initials input
+
+// Function for quiz
+// Function for displaying question
+//  -- Display correct/incorrect
+
+// Create a try again button/clear high score button
+
+
+
