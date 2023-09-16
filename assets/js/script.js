eed28bfe03a6cff8c89335d3c654aae2512d0cf9
kittykatt210 <keperry30@icloud.com>
2023-09-16 17:15:08 -0500

    Started working on the javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,92 @@
 // Get a handle on all html tags, objects, classes, and identifiers
 // console.log everything to ensure it works
+var welcomeScreen = document.querySelector('.welcome');
+var timer = document.querySelector('.time');
+var stButton = document.getElementById('begin');
 
 // Create questions object
 //  -- Contains the question and correct answer
+ var questions = [{
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            { text: "&ltscript&gt", isCorrect: true},
+            { text: "&ltjs&gt", isCorrect: false},
+            { text: "&ltscripting&gt", isCorrect: false},
+            { text: "&ltjavascript&gt", isCorrect: false}
+        ]
+    },
+    {
+        question: "What is the correct JS syntax to change the content of the HTML element: &ltp id='demo'&gt This is a demonstration. &lt/p&gt",
+        answers: [
+            { text: 'document.getElementByName("p").innerHTML = "Hello World.";', isCorrect: false},
+            { text: 'document.getElementById("demo").innerHTML = "Hello World.";', isCorrect: true},
+            { text: 'document.getElement("p").innerHTML = "Hello World.";', isCorrect: false},
+            { text: '#demo.innerHTML = "Hello World.";', isCorrect: false}
+        ]
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: [
+            { text: 'msgBox("Hello World");', isCorrect: false},
+            { text: 'alertBox("Hello World");', isCorrect: false},
+            { text: 'alert("Hello World");', isCorrect: true},
+            { text: 'msg("Hello World");', isCorrect: false}
+        ]
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: [
+            { text: 'if i = 5', isCorrect: false},
+            { text: 'if (i == 5)', isCorrect: true},
+            { text: 'if i = 5 then', isCorrect: false},
+            { text: 'if i == 5 then', isCorrect: false}
+        ]
+    },
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        answers: [
+            { text: 'if i =! 5 then', isCorrect: false},
+            { text: 'if i &lt&gt 5', isCorrect: false},
+            { text: 'if (i &lt&gt 5)', isCorrect: false},
+            { text: 'if (i != 5)', isCorrect: true}
+        ]
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            { text: 'Both the &lthead&gt section and the &ltbody&gt section are correct', isCorrect: true},
+            { text: 'The &lthead&gt section', isCorrect: false},
+            { text: 'The &ltbody&gt section',  isCorrect: false},
+            { text: 'Neither the &lthead&gt section nor the &ltbody&gt section are correct', isCorrect: false}
+        ]
+    }];
+var questionEl = document.getElementById("question");
+var answerBt = document.getElementById("answer-buttons");
 
+var score = 0;
+var qIndex = 0;
+
+function showQuestion() {
+    reset();
+    var currQuest = questions[qIndex];
+    questionEl.innerHTML = currQuest.question;
+    
+    currQuest.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerBt.appendChild(button);
+    });
+}
+
+function reset() {
+    while(answerBt.firstChild){
+        answerBt.removeChild(answerBt.firstChild);
+    }
+}
+
+
+showQuestion();
 // Save answers to local storage
 //  -- Calculate score
 // Create a timer -- 10 seconds per question
